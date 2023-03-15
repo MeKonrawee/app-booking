4e9f53a5254997d0f0ab14339b9a350d1f9ee0d4
MeKonrawee <konrawee207@gmail.com>
2023-03-15 17:19:44 +0700

    Revert "Merge pull request #1 from dudkinox/master"
    This reverts commit bfa127b60a1d591c9f5f4d170798a09ad9c09de7, reversing
    changes made to 9eef075e7213f895260b6fecd82bc29832072003.

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -3,7 +3,6 @@ const { RegisterService: Register } = require("../controllers/loginController");
 const { LoginService: Login } = require("../controllers/loginController");
 const {
   InsertMenuService: insertMenu,
-  GetMenuService: getMenu,
 } = require("../controllers/menuController");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDoc = require("../swagger");
@@ -15,7 +14,6 @@ router.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 router.post("/register", Register);
 router.post("/login", Login);
 router.post("/menus", insertMenu);
-router.get("/menus", getMenu);
 
 module.exports = {
   routes: router,
