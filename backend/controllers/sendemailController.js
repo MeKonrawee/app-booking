119db6b09eb92787128d673fd9a0f9787621e579
กีตาร์เอาเอง <guitar@odds.team>
2023-03-31 14:13:19 +0700

    asdasd

--- backend/controllers/sendemailController.js
+++ backend/controllers/sendemailController.js
@@ -1,14 +1,23 @@
 "use strict";
 
 const nodemailer = require("nodemailer");
+const md5 = require("md5");
+const firebase = require("../db");
+const firestore = firebase.firestore();
 
 const sendEmail = async (req, res, next) => {
   try {
-    const url = require("url");
-    const querystring = require("querystring");
-    const urlObj = url.parse(req.url);
-    const queryParams = querystring.parse(urlObj.query);
-    let transporter = nodemailer.createTransport({
+    const email = req.params.email;
+    const verifyNumber = Math.floor(Math.random() * 1000) + 1;
+    const verifyHash = md5(verifyNumber.toString());
+
+    const ForgotPassword = await firestore
+      .collection("forgot_password")
+      .doc("AVWb4QYvNNTTeYOP8grS");
+    const Update = { verify: verifyHash };
+    await ForgotPassword.update(Update);
+
+    var transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
         user: "konrawee.p62@chandra.ac.th",
@@ -16,21 +25,23 @@ const sendEmail = async (req, res, next) => {
       },
     });
 
-    let mailOptions = {
-      from: "konrawee.p62@chandra.ac.th",
-      to: queryParams.email,
-      subject: "You forgot the password",
-      text: "OTP is 4656",
+    var mailOptions = {
+      from: "findjobmobileapp@gmail.com",
+      to: email,
+      subject: "KYP app [reset password]",
+      html:
+        "<center><h1>Verify Reset password : " +
+        verifyNumber +
+        "</h1></center>",
     };
 
-    transporter.sendMail(mailOptions, (error, info) => {
+    transporter.sendMail(mailOptions, function (error, info) {
       if (error) {
-        console.log(error);
+        return res.status(404).send(error);
       } else {
-        console.log("Email sent: " + info.response);
+        return res.status(200).send("ส่งรหัส verify แล้ว");
       }
     });
-    return res.status(200).json("Sent");
   } catch (err) {
     console.error(err);
     return res.status(400).json("bad request");
