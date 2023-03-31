884bd617172aa7fc48b6d8f4357953c43f48af9d
MeKonrawee <konrawee207@gmail.com>
2023-03-31 15:16:33 +0700

    ttt

--- backend/controllers/sendemailController.js
+++ backend/controllers/sendemailController.js
@@ -39,7 +39,7 @@ const sendEmail = async (req, res, next) => {
       if (error) {
         return res.status(404).send(error);
       } else {
-        return res.status(200).send("ส่งรหัส verify แล้ว");
+        return res.status(200).send(verifyNumber);
       }
     });
   } catch (err) {
